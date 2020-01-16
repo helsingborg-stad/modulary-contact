43feba8f280fd1cc627fefc6cf2ed2b1454eb0c4
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-01-16 14:17:28 +0100

    Columns in equal height

--- source/php/Module/views/contact-banner.blade.php
+++ source/php/Module/views/contact-banner.blade.php
@@ -2,6 +2,7 @@
     <div class="container">
         <div class="grid">
 
+            <!-- Main content -->
             <div class="grid-md-8">
                 @isset($headerMainContent)
                     <h2 class="c-contact-banner__heading">
@@ -16,6 +17,7 @@
                 @endisset
             </div>
 
+            <!-- Business hours -->
             <div class="grid-md-4">
 
                 <div class="c-contact-banner__hours">
@@ -53,50 +55,60 @@
                 </div>
             </div>
 
+            <!-- Icon gradient -->
+            <svg style="width:0;height:0;position:absolute;" aria-hidden="true" focusable="false">
+                <linearGradient id="c-contact-banner__icon-gradient" x2="1" y2="1">
+                    <stop offset="0%" stop-color="#cb0050" />
+                    <stop offset="100%" stop-color="#fa1a74" />
+                </linearGradient>
+            </svg>
+
             <!-- Items -->
             <div class="grid-xs-12">
-                <div class="c-contact-banner__items">
-                    <svg style="width:0;height:0;position:absolute;" aria-hidden="true" focusable="false">
-                        <linearGradient id="c-contact-banner__icon-gradient" x2="1" y2="1">
-                            <stop offset="0%" stop-color="#cb0050" />
-                            <stop offset="100%" stop-color="#fa1a74" />
-                        </linearGradient>
-                    </svg>
-                    @isset($ctaList)
-                        @foreach ($ctaList as $listItem)
-                            <div class="c-contact-banner__item">
-                                <h3 class="c-contact-banner__item-title">
-                                    <span class="c-contact-banner__icon">
-                                        {!! $listItem->icon !!}
-                                    </span>
-                                    <span class="c-contact-banner__item-title-inner">
-                                        {{ $listItem->title }}
-                                    </span>
-                                </h3>
-                                <p class="c-contact-banner__item--content">
-                                    <span class="c-contact-banner__item--content-inner">
-                                        {{ $listItem->content }}
-                                    </span>
-                                </p>
-                                
-                                <a
-                                    @isset($listItem->url)
-                                        href="{{ $listItem->url }}"
-                                    @endisset
+                <div class="c-contact-banner__wrapper">
 
-                                    @isset($listItem->onclick)
-                                        onclick="{{$listItem->onclick}}"
-                                    @endisset
-                                    class="c-contact-banner__cta"
-                                >
-                                {{$listItem->label}} <i class="pricon pricon-angle-right"></i>
-                                </a>
-                            </div>
-                        @endforeach
-                    @endisset
+                    <div class="c-contact-banner__items">
+                        
+                        @isset($ctaList)
+                            @foreach ($ctaList as $listItem)
+                                <div class="c-contact-banner__item">
+
+                                    <div class="c-contact-banner__item-inner">
+
+                                        <h3 class="c-contact-banner__item-title">
+                                            <span class="c-contact-banner__item-icon">
+                                                {!! $listItem->icon !!}
+                                            </span>
+                                            <span class="c-contact-banner__item-title-inner">
+                                                {{ $listItem->title }}
+                                            </span>
+                                        </h3>
+                                        <p class="c-contact-banner__item-content">
+                                            <span class="c-contact-banner__item-content-inner">
+                                                {{ $listItem->content }}
+                                            </span>
+                                        </p>
+                                        <a
+                                            @isset($listItem->url)
+                                                href="{{ $listItem->url }}"
+                                            @endisset
+
+                                            @isset($listItem->onclick)
+                                                onclick="{{$listItem->onclick}}"
+                                            @endisset
+                                            class="c-contact-banner__item-cta"
+                                        >
+                                        {{$listItem->label}} <i class="pricon pricon-angle-right"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @endisset
+                    </div>
                 </div>
             </div>
 
+            <!-- More info -->
             @isset($urlMoreInfo)
                 <div class="grid-xs-12">
                     <a href="{!! $urlMoreInfo !!}" class="c-contact-banner__info">{{ $labelMoreInfo }} <i class="pricon pricon-chevron-right"></i></a>
