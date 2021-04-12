618978addc345ca1ebf1c6bd7d5fe001cc2d37b7
Eric Rosenborg <eric.rosenborg@hiq.se>
2021-04-12 13:44:45 +0200

    Add padding

--- source/php/Module/views/contact-banner.blade.php
+++ source/php/Module/views/contact-banner.blade.php
@@ -5,7 +5,10 @@
             @foreach ($ctaList as $index => $listItem)
                 @card([
                     'classList' => [
-                        'c-card--panel', 'card-item-'.$index
+                        'c-card--panel', 'card-item-'.$index, 'u-padding__left--2',
+                        'u-padding__right--2',
+                        'u-padding__top--2',
+                        'u-padding__bottom--2'
                     ],
                         'attributeList' => [
                         'aria-labelledby' => 'mod-contactbanner-' . $id
@@ -14,20 +17,23 @@
 
                     <div class="c-card__header">
                         @icon([
-                            'icon' => $listItem->icon, 'size' => 'md'
+                            'icon' => $listItem->icon, 
+                            'size' => 'md',
+                            'color' => 'primary'
                         ])
                         @endicon
     
                         @typography([
                             'element' => 'h4',
                             'variant' => 'p',
-                            'id'      => 'mod-contactbanner-' . $id
+                            'id'      => 'mod-contactbanner-' . $id,
+                            'classList' => ['u-color__text--info', 'u-margin__left--2']
                         ])
     
                         {{$listItem->title}}
                         @endtypography
                     </div>
-                    <div class="c-card__body">
+                    <div class="c-card__body u-padding__left--2 u-padding__top--2 u-padding__bottom--2 u-padding__right--2">
                         {!! $listItem->content !!}
                     </div>
   
