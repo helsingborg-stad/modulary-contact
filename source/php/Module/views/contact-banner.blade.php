0b05c5b8002d015b7f61b97b4f630d1130590057
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-04-14 10:14:19 +0200

    Changed markup and added functionality for script

--- source/php/Module/views/contact-banner.blade.php
+++ source/php/Module/views/contact-banner.blade.php
@@ -5,17 +5,15 @@
             @foreach ($ctaList as $index => $listItem)
                 @card([
                     'classList' => [
-                        'c-card--panel', 'card-item-'.$index, 'u-padding__left--2',
-                        'u-padding__right--2',
-                        'u-padding__top--2',
-                        'u-padding__bottom--2'
+                        'card-item-'.$index,
                     ],
                         'attributeList' => [
                         'aria-labelledby' => 'mod-contactbanner-' . $id
-                    ]
+                    ],
+                    'context' => 'contactbanner'
                 ])
 
-                    <div class="c-card__header">
+                    <div class="c-card__header u-padding__x--4 u-padding__top--3">
                         @icon([
                             'icon' => $listItem->icon, 
                             'size' => 'md',
@@ -27,14 +25,35 @@
                             'element' => 'h4',
                             'variant' => 'p',
                             'id'      => 'mod-contactbanner-' . $id,
-                            'classList' => ['u-color__text--info', 'u-margin__left--2']
+                            'classList' => ['u-margin__left--2']
                         ])
     
                         {{$listItem->title}}
                         @endtypography
                     </div>
-                    <div class="c-card__body u-padding__left--2 u-padding__top--2 u-padding__bottom--2 u-padding__right--2">
+
+                    <div class="c-card__body u-padding--4">
                         {!! $listItem->content !!}
+                    </div>
+
+                    <div class="c-card__footer c-contact-banner__footer u-padding--2">
+                        @button([
+                            'text' => $listItem->label,
+                            'color' => 'default',
+                            'style' => 'basic',
+                            'icon' => 'arrow_forward',
+                            'href' => !empty($listItem->url) ? $listItem->url : '',
+                            'attributeList' => [
+                                'onclick' => !empty($listItem->onclick) ? 'action'.$index.'()' : ''
+                            ]
+                        ])
+                        @endbutton
+
+                        @if (!empty($listItem->onclick))
+                        <script>function action{!! $index !!}() {
+                            {!! $listItem->onclick !!}
+                        }</script>
+                        @endif
                     </div>
   
                 @endcard
