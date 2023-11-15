498d0d893c201a9cff7fb1c4d57ab072947f34ad
GitHub Action <>
2023-11-15 10:29:27 +0000

    build: update package file versions to 3.1.0

--- modularity-contact-banner.php
+++ modularity-contact-banner.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Modularity Contact Banner
  * Plugin URI:        https://github.com/helsingborg-stad/modularity-contact
  * Description:       Modularity Contact Banner
- * Version: 3.0.2
+ * Version: 3.1.0
  * Author:            Sebastian Thulin
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
